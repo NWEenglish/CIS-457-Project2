275c248a1b4c1330695bc51c8a178bd939b40834
DenverDeBoer <32941698+DenverDeBoer@users.noreply.github.com>
2019-11-11 19:17:37 -0500

    Ftp commands for GUI

--- GUI.py
+++ GUI.py
@@ -1,13 +1,14 @@
 #GUI
 import tkinter
 import socket
+import select
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import messagebox
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Connects to server
+#Connects to central server
 #Error checks user input
 #Sends username, hostname, and connection speed to server
 def connectServer():
@@ -19,19 +20,90 @@ def connectServer():
     if len(servIP) > 0 and len(port) > 0 and len(username) > 0 and len(hostname) > 0 and len(speed) > 0:
         serverAddress = (str(servIP), int(port))
         sock.connect(serverAddress)
-        messagebox.showinfo("Connection", "Connected to Central Server")
+        messagebox.showinfo("Connection", "Connected to central server")
         msg = str(username) + " " + str(hostname) + " " + str(speed)
         sock.sendall(msg.encode())
     else:
-        messagebox.showerror("Invalid Input", "Please Fill In All Fields")
+        messagebox.showerror("Invalid Input", "Please fill in all fields")
 
+#Keyword Search
+#Sends keyword to the central server and displays feedback in modified treeview widget
+def keywordSearch():
+    key = searchText.get()
+    if len(key) > 0:
+        sock.sendall(key.encode())
+    else:
+        messagebox.showerror("Invalid Input", "Please enter in a keyword")
+
+#Connection with Remote Host
+#Host can connect, quit, and retrieve files from the remote host
+def remoteHost():
+    command = ftpText.get()
+    if len(command) > 0:
+        command = command.split()
+        if command[0].upper() == "CONNECT" and len(command) == 3:
+            ftpResult.insert(tkinter.INSERT, "->" + ftpText.get() + "\n")
+            ftpResult.insert(tkinter.INSERT, "Connecting...\n")
+            try:
+                ftpAddress = (str(command[1]), int(command[2]))
+                sock.connect(ftpAddress)
+                ftpResult.insert(tkinter.INSERT, "Connected to " + command[1] + " on port " + command[2] + "\n\n")
+            except:
+                ftpResult.insert(tkinter.INSERT, "Connection Failed\n\n")
+                messagebox.showerror("FTP ERROR", "Could not connect to the server")
+        elif command[0].upper() == "RETRIEVE" and len(command) == 2:
+            ftpResult.insert(tkinter.INSERT, "->" + ftpText.get() + "\n")
+            ftpResult.insert(tkinter.INSERT, "Retrieving File....\n")
+            try:
+                msg = "RETRIEVE " + command[1]
+                sock.sendall(msg.encode())
+                
+                file = open(command[1], 'w')
+                totalData = []
+                data = ''
+                
+                while (True):
+                    ready = select.select([sock], [], [], 2)
+                    if (ready[0]):
+                        data = sock.recv(1024).decode()
+                    else:
+                        break
+                        
+                    totalData.append(data)
+                    file.write(''.join(totalData))
+                    file.close()
+                    ftpResult.insert(tkinter.INSERT, "Retrieved " + command[1] + "\n\n")
+            except:
+                ftpResult.insert(tkinter.INSERT, "Retrieval Failed\n\n")
+                messagebox.showerror("FTP ERROR", "Could not retrieve file from the server")
+        elif command[0].upper() == "QUIT":
+            ftpResult.insert(tkinter.INSERT, "->" + ftpText.get() + "\n")
+            ftpResult.insert(tkinter.INSERT, "Terminating Connection....\n")
+            try:
+                msg = "QUIT"
+                sock.sendall(msg.encode())
+                sock.close()
+                ftpResult.insert(tkinter.INSERT, "Connection Terminated\n\n")
+            except:
+                ftpResult.insert(tkinter.INSERT, "Termination Failed\n\n")
+                messagebox.showerror("FTP ERROR", "Could not terminate connection to the server")
+        elif command[0].upper() == "HELP":
+            ftpResult.insert(tkinter.INSERT, "->" + ftpText.get() + "\n")
+            ftpResult.insert(tkinter.INSERT, "CONNECT <SERVER NAME/IP ADDRESS> <SERVER PROT>\n")
+            ftpResult.insert(tkinter.INSERT, "RETRIEVE <FILENAME>\n")
+            ftpResult.insert(tkinter.INSERT, "QUIT\n")
+            ftpResult.insert(tkinter.INSERT, "HELP\n\n")
+        else:
+            messagebox.showerror("Invalid Command", "Please enter a command or type HELP")
+    else:
+        messagebox.showerror("Invalid Command", "Please enter a command or type HELP")
 
 gui = tkinter.Tk()
 gui.title("NAP Host GUI")
-gui.geometry("700x525")
+gui.geometry("750x525")
 
 ##################### CONNECTING TO CENTRAL SERVER ###########################
-cLabel = tkinter.Label(gui, text = "Connection", font = "bold")
+cLabel = tkinter.Label(gui, text = "Connection", font = ("-weight bold", 13))
 cLabel.grid(column = 0, row = 0, padx = 10, sticky = "W", columnspan = 6)
 
 serverHostLabel = tkinter.Label(gui, text = "Server Hostname: ")
@@ -66,31 +138,37 @@ speedOps.grid(column = 5, row = 2, sticky = "W")
 ##############################################################################
 
 ######################### KEYWORD SEARCHING ##################################
-sLabel = tkinter.Label(gui, text = "Keyword Search", font = "bold")
-sLabel.grid(column = 0, row = 3, pady = 15, padx = 10, sticky = "W")
+sLabel = tkinter.Label(gui, text = "Keyword Search", font = ("-weight bold", 13))
+sLabel.grid(column = 0, row = 3, pady = 10, padx = 10, sticky = "W")
 
 searchLabel = tkinter.Label(gui, text = "Search: ")
 searchLabel.grid(column = 0, row = 4)
 searchText = tkinter.Entry(gui, width = 50)
 searchText.grid(column = 1, row = 4, sticky = "W", columnspan = 3)
 
-searchButton = tkinter.Button(gui, text = "Search", width = 20)
+searchButton = tkinter.Button(gui, text = "Search", width = 20, command = keywordSearch)
 searchButton.grid(column = 4, row = 4, columnspan = 2)
 
-searchResult = scrolledtext.ScrolledText(gui, width = 75, height = 7)
-searchResult.grid(column = 0, row = 5, pady = 10, padx = 30, columnspan = 6, rowspan = 5)
+searchResult = ttk.Treeview(gui, columns=("hostname", "filename"), height = 5)
+searchResult.column("#0", width = 120, minwidth = 120, stretch = tkinter.NO)
+searchResult.heading("#0", text="Speed", anchor=tkinter.W)
+searchResult.column("hostname", width = 260, minwidth = 260, stretch = tkinter.NO)
+searchResult.heading("hostname", text="Hostname", anchor=tkinter.W)
+searchResult.column("filename", width = 250, minwidth = 250, stretch = tkinter.NO)
+searchResult.heading("filename", text="Filename", anchor=tkinter.W)
+searchResult.grid(column = 0, row = 5, pady = 10, padx = 20, columnspan = 10, rowspan = 5)
 ##############################################################################
 
 ############################# FTP COMMANDS ###################################
-fLabel = tkinter.Label(gui, text = "FTP", font = "bold")
-fLabel.grid(column = 0, row = 10, pady = 15, padx = 10, sticky = "W")
+fLabel = tkinter.Label(gui, text = "FTP", font = ("-weight bold", 13))
+fLabel.grid(column = 0, row = 10, pady = 10, padx = 10, sticky = "W")
 
 ftpLabel = tkinter.Label(gui, text = "Enter Command: ")
 ftpLabel.grid(column = 0, row = 11)
 ftpText = tkinter.Entry(gui, width = 50)
 ftpText.grid(column = 1, row = 11, sticky = "W", columnspan = 3)
 
-ftpButton = tkinter.Button(gui, text = "Enter", width = 20)
+ftpButton = tkinter.Button(gui, text = "Enter", width = 20, command = remoteHost)
 ftpButton.grid(column = 4, row = 11, columnspan = 2)
 
 ftpResult = scrolledtext.ScrolledText(gui, width = 75, height = 7)
